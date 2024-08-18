498025fe6c061150f9aae0634c2c6791fb955a0f
hyuk-dev <hyuk.development@gmail.com>
2024-08-18 21:22:44 +0900

    쓰레드

---
+++ programmers-java/ThreadExam1.java
@@ -0,0 +1,11 @@
+public class ThreadExam1 {
+    public static void main(String[] args){
+        MyThread1 t1 = new MyThread1("*");
+        MyThread1 t2 = new MyThread1("-");
+
+        t1.start();
+        t2.start();
+
+        System.out.println("!");
+    }
+}
